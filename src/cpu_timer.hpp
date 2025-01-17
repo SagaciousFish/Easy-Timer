c18395a6a8342b39fe1c409cdce7032fea474224
Zeyu Liu <274756738@qq.com>
2025-01-17 12:56:34 +0800

    Latest

--- src/cpu_timer.hpp
+++ src/cpu_timer.hpp
@@ -4,53 +4,67 @@
 
 using namespace std::chrono;
 
-namespace easy_timer
-{
+#define RAII_TIMER_F(name) easy_timer::CpuTimer timer(name);
+#define RAII_TIMER() easy_timer::CpuTimer timer(__FILE__ ":" __LINE__ ":" __func__);
+
+namespace easy_timer {
     // Clock Selection from https://stackoverflow.com/questions/38252022/does-standard-c11-guarantee-that-high-resolution-clock-measure-real-time-non
     // The implementation chooses library-preferred steady clock, or the most precise built-in clock
     using PreciseBuiltinClock =
     std::conditional_t<
         system_clock::period::den <= steady_clock::period::den,
         system_clock, steady_clock
-      >;
+    >;
     using TimerClock =
-        std::conditional_t<
-            high_resolution_clock::is_steady,
-            high_resolution_clock, PreciseBuiltinClock
-          >;
+    std::conditional_t<
+        high_resolution_clock::is_steady,
+        high_resolution_clock, PreciseBuiltinClock
+    >;
 
 
-    class CpuTimer
-    {
-    protected:
-        bool stopped_;
-        time_point<TimerClock> tp_start_;
-        time_point<TimerClock> tp_end_;
+    class CpuTimer {
+        protected:
+            bool stopped_;
+            time_point<TimerClock> tp_start_;
+            time_point<TimerClock> tp_end_;
+            const char *name_{};
 
-    public:
-        CpuTimer() : stopped_(true)
-        {
-            tp_start_ = TimerClock::now();
-            tp_end_ = TimerClock::now();
-        }
-        void Start()
-        {
-            if (!stopped_)
-                return;
-            stopped_ = false;
-            tp_start_ = TimerClock::now();
-        }
-        void Stop()
-        {
-            stopped_ = true;
-            tp_end_ = TimerClock::now();
-        }
-        float GetMillis()
-        {
-            if (!stopped_)
-                tp_end_ = TimerClock::now();
-            const float elapsed_time = duration<float, std::milli>(tp_end_ - tp_start_).count();
-            return elapsed_time;
-        }
+        public:
+            CpuTimer()
+                : stopped_(true) {
+                tp_end_   = TimerClock::now();
+                tp_start_ = TimerClock::now();
+            }
+
+            explicit CpuTimer(const char *name)
+                : CpuTimer() {
+                name_ = name;
+                this->Start();
+            }
+
+            ~CpuTimer() {
+                if (!stopped_) {
+                    printf("[RAII] %s: %.5f ms\n", name_, this->GetMillis());
+                }
+            }
+
+
+            void Start() {
+                if (!stopped_)
+                    return;
+                stopped_  = false;
+                tp_start_ = TimerClock::now();
+            }
+
+            void Stop() {
+                stopped_ = true;
+                tp_end_  = TimerClock::now();
+            }
+
+            float GetMillis() {
+                tp_end_                  = TimerClock::now();
+                const float elapsed_time = duration<float, std::milli>(tp_end_ - tp_start_).count();
+                return elapsed_time;
+            }
     };
 }
